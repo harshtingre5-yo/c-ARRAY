45597189565d15d7633a783ffc4882a3ebea2fc3
harshtingre5-yo <harshtingre5@gmail.com>
2026-08-20 14:42:44 +0530

    Add program to find the smallest number in an array

---
+++ find smallest no using array.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    int arr[100], n, i, smallest;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter %d numbers:\n", n);
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    smallest = arr[0];
+
+    for (i = 1; i < n; i++) {
+        if (arr[i] < smallest) {
+            smallest = arr[i];
+        }
+    }
+
+    printf("Smallest number = %d\n", smallest);
+
+    return 0;
+}
